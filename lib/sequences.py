dadb2b21b82ec1c3cafa6c2f1c4d40446df4b752
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-12-10 18:10:37 +0300

    Completed Assignement

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    f = 0
+    g = 1
+
+    if length<0:
+        return []
+    elif length == 0:
+        return [] 
+    elif length == 1:
+        return g 
+    else: 
+        for i in range (2, length):
+            j = f + g 
+            f = g 
+            g = j 
+        return g 
+
+print_fibonacci(9)
+
